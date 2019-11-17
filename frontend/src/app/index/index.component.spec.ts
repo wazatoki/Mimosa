cd1af2c03de5d9e0ab297c6e5d686ca1b71bf9dd
wazatoki <wazatoki@gmail.com>
2019-11-17 12:45:46 +0900

    indexコンポーネント作成

---
+++ frontend/src/app/index/index.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatListModule } from '@angular/material/list';
+import { IndexComponent } from './index.component';
+import { HeaderComponent } from '../header/header.component';
+import { LayoutHeaderContentsComponent } from '../layout-header-contents/layout-header-contents.component';
+
+describe('IndexComponent', () => {
+  let component: IndexComponent;
+  let fixture: ComponentFixture<IndexComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        IndexComponent,
+        HeaderComponent,
+        LayoutHeaderContentsComponent
+      ],
+      imports: [
+        MatListModule,
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IndexComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  fit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
